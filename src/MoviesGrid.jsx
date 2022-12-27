93e468dfecedc0daae8b5be08fc51736a6b15c35
Sol <solabigail00@hotmail.com>
2022-12-26 21:57:23 -0300

    MovieList
    Remove files.
    Add json with movies
    Add movie list

---
+++ src/MoviesGrid.jsx
@@ -0,0 +1,12 @@
+import movies from './movies.json';
+
+export function MoviesGrid() {
+    console.log(movies)
+  return (
+    <ul>
+      {movies.map(function(movie){
+        return <li>{movie.title}</li>
+      })}
+    </ul>
+  );
+}
